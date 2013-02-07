cbef639b072c983a44d89980fafd50f045078d64
hohll <hohll@users.noreply.github.com>
2013-02-07 15:26:42 +0000

    LUTECE-1493 : Bootstrap CSS Framework integration profiles: Update several templates back + init_db_profile.sql
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/system/plugin-profiles@46883 bab10101-e421-0410-a517-8ce0973de3ef

--- src/sql/plugins/profiles/plugin/init_db_profile.sql
+++ src/sql/plugins/profiles/plugin/init_db_profile.sql
@@ -1,21 +1,15 @@
 --
 -- Init  table profile_action
 --
-INSERT INTO profile_action( id_action,name_key,description_key,action_url,icon_url,action_permission) 
-	VALUES(1,'profiles.action.modify_profile.name','profiles.action.modify_profile.description','jsp/admin/plugins/profiles/ModifyProfile.jsp','images/admin/skin/actions/modify.png','MODIFY_PROFILE');
-INSERT INTO profile_action( id_action,name_key,description_key,action_url,icon_url,action_permission) 
-	VALUES(2,'profiles.action.delete_profile.name','profiles.action.delete_profile.description','jsp/admin/plugins/profiles/RemoveProfile.jsp','images/admin/skin/actions/delete.png','DELETE_DELETE');
-INSERT INTO profile_action( id_action,name_key,description_key,action_url,icon_url,action_permission) 
-	VALUES(3,'profiles.action.manage_users_assignment.name','profiles.action.manage_users_assignment.description','jsp/admin/plugins/profiles/AssignUsersProfile.jsp','images/admin/skin/actions/group.png','MANAGE_USERS_ASSIGNMENT');
-INSERT INTO profile_action( id_action,name_key,description_key,action_url,icon_url,action_permission) 
-	VALUES(4,'profiles.action.manage_rights_assignment.name','profiles.action.manage_rights_assignment.description','jsp/admin/plugins/profiles/AssignRightsProfile.jsp','images/admin/skin/actions/right.png','MANAGE_RIGHTS_ASSIGNMENT');
-INSERT INTO profile_action( id_action,name_key,description_key,action_url,icon_url,action_permission) 
-	VALUES(5,'profiles.action.manage_roles_assignment.name','profiles.action.manage_roles_assignment.description','jsp/admin/plugins/profiles/AssignRolesProfile.jsp','images/admin/skin/actions/rbac.png','MANAGE_ROLES_ASSIGNMENT');
-INSERT INTO profile_action( id_action,name_key,description_key,action_url,icon_url,action_permission) 
-	VALUES(6,'profiles.action.manage_workgroups_assignment.name','profiles.action.manage_workgroups_assignment.description','jsp/admin/plugins/profiles/AssignWorkgroupsProfile.jsp','images/admin/skin/actions/group.png','MANAGE_WORKGROUPS_ASSIGNMENT');
-INSERT INTO profile_action( id_action,name_key,description_key,action_url,icon_url,action_permission) 
-	VALUES(7,'profiles.action.manage_view_assignment.name','profiles.action.manage_view_assignment.description','jsp/admin/plugins/profiles/AssignViewProfile.jsp','images/admin/skin/plugins/profiles/actions/views.png','MANAGE_VIEW_ASSIGNMENT');
-
+INSERT INTO profile_action (id_action, name_key, description_key, action_url, icon_url, action_permission) VALUES (1,'profiles.action.modify_profile.name','profiles.action.modify_profile.description','jsp/admin/plugins/profiles/ModifyProfile.jsp','icon-edit','MODIFY_PROFILE');
+INSERT INTO profile_action (id_action, name_key, description_key, action_url, icon_url, action_permission) VALUES (2,'profiles.action.delete_profile.name','profiles.action.delete_profile.description','jsp/admin/plugins/profiles/RemoveProfile.jsp','icon-trash','DELETE_DELETE');
+INSERT INTO profile_action (id_action, name_key, description_key, action_url, icon_url, action_permission) VALUES (3,'profiles.action.manage_users_assignment.name','profiles.action.manage_users_assignment.description','jsp/admin/plugins/profiles/AssignUsersProfile.jsp','icon-user','MANAGE_USERS_ASSIGNMENT');
+INSERT INTO profile_action (id_action, name_key, description_key, action_url, icon_url, action_permission) VALUES (4,'profiles.action.manage_rights_assignment.name','profiles.action.manage_rights_assignment.description','jsp/admin/plugins/profiles/AssignRightsProfile.jsp','icon-lock','MANAGE_RIGHTS_ASSIGNMENT');
+INSERT INTO profile_action (id_action, name_key, description_key, action_url, icon_url, action_permission) VALUES (5,'profiles.action.manage_roles_assignment.name','profiles.action.manage_roles_assignment.description','jsp/admin/plugins/profiles/AssignRolesProfile.jsp','icon-th-list','MANAGE_ROLES_ASSIGNMENT');
+INSERT INTO profile_action (id_action, name_key, description_key, action_url, icon_url, action_permission) VALUES (6,'profiles.action.manage_workgroups_assignment.name','profiles.action.manage_workgroups_assignment.description','jsp/admin/plugins/profiles/AssignWorkgroupsProfile.jsp','icon-x-group icon-x-white','MANAGE_WORKGROUPS_ASSIGNMENT');
+INSERT INTO profile_action (id_action, name_key, description_key, action_url, icon_url, action_permission) VALUES (7,'profiles.action.manage_view_assignment.name','profiles.action.manage_view_assignment.description','jsp/admin/plugins/profiles/AssignViewProfile.jsp','icon-eye-open','MANAGE_VIEW_ASSIGNMENT');
+	
+	
 --
 -- Init  table profile_view_action
 --	
--- src/sql/plugins/profiles/upgrades/upgrade_db_profiles-1.0.3-1.0.4.sql
+++ src/sql/plugins/profiles/upgrades/upgrade_db_profiles-1.0.3-1.0.4.sql
@@ -1,14 +1,13 @@
-DROP TABLE IF EXISTS profile_view_action;
-CREATE TABLE IF NOT EXISTS profile_view_action (
-  id_action int(11) NOT NULL DEFAULT '0',
-  name_key varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  description_key varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  action_url varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  icon_url varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  action_permission varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (id_action)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+DELETE FROM profile_action;
+INSERT INTO profile_action (id_action, name_key, description_key, action_url, icon_url, action_permission) VALUES (1,'profiles.action.modify_profile.name','profiles.action.modify_profile.description','jsp/admin/plugins/profiles/ModifyProfile.jsp','icon-edit','MODIFY_PROFILE');
+INSERT INTO profile_action (id_action, name_key, description_key, action_url, icon_url, action_permission) VALUES (2,'profiles.action.delete_profile.name','profiles.action.delete_profile.description','jsp/admin/plugins/profiles/RemoveProfile.jsp','icon-trash','DELETE_DELETE');
+INSERT INTO profile_action (id_action, name_key, description_key, action_url, icon_url, action_permission) VALUES (3,'profiles.action.manage_users_assignment.name','profiles.action.manage_users_assignment.description','jsp/admin/plugins/profiles/AssignUsersProfile.jsp','icon-user','MANAGE_USERS_ASSIGNMENT');
+INSERT INTO profile_action (id_action, name_key, description_key, action_url, icon_url, action_permission) VALUES (4,'profiles.action.manage_rights_assignment.name','profiles.action.manage_rights_assignment.description','jsp/admin/plugins/profiles/AssignRightsProfile.jsp','icon-lock','MANAGE_RIGHTS_ASSIGNMENT');
+INSERT INTO profile_action (id_action, name_key, description_key, action_url, icon_url, action_permission) VALUES (5,'profiles.action.manage_roles_assignment.name','profiles.action.manage_roles_assignment.description','jsp/admin/plugins/profiles/AssignRolesProfile.jsp','icon-th-list','MANAGE_ROLES_ASSIGNMENT');
+INSERT INTO profile_action (id_action, name_key, description_key, action_url, icon_url, action_permission) VALUES (6,'profiles.action.manage_workgroups_assignment.name','profiles.action.manage_workgroups_assignment.description','jsp/admin/plugins/profiles/AssignWorkgroupsProfile.jsp','icon-x-group icon-x-white','MANAGE_WORKGROUPS_ASSIGNMENT');
+INSERT INTO profile_action (id_action, name_key, description_key, action_url, icon_url, action_permission) VALUES (7,'profiles.action.manage_view_assignment.name','profiles.action.manage_view_assignment.description','jsp/admin/plugins/profiles/AssignViewProfile.jsp','icon-eye-open','MANAGE_VIEW_ASSIGNMENT');
 
+DELETE FROM profile_view_action;
 INSERT INTO profile_view_action (id_action, name_key, description_key, action_url, icon_url, action_permission) VALUES (1, 'profiles.action.modify_view.name', 'profiles.action.modify_view.description', 'jsp/admin/plugins/profiles/ModifyView.jsp', 'icon-edit', 'MODIFY_VIEW');
 INSERT INTO profile_view_action (id_action, name_key, description_key, action_url, icon_url, action_permission) VALUES (2, 'profiles.action.delete_view.name', 'profiles.action.delete_view.description', 'jsp/admin/plugins/profiles/RemoveView.jsp', 'icon-trash', 'DELETE_VIEW');
 INSERT INTO profile_view_action (id_action, name_key, description_key, action_url, icon_url, action_permission) VALUES (3, 'profiles.action.manage_views_assignment.name', 'profiles.action.manage_views_assignment.description', 'jsp/admin/plugins/profiles/AssignProfilesView.jsp', 'icon-folder-close', 'MANAGE_PROFILES_ASSIGNMENT');
