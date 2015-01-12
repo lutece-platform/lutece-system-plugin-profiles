fcf065e73742a321b4b8bc0eb2e35fae2c161c12
hohll <hohll@users.noreply.github.com>
2015-01-12 14:42:49 +0000

    PROFILES-18 Add adminlte theme for back-office
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/system/plugin-profiles@73196 bab10101-e421-0410-a517-8ce0973de3ef

---
+++ src/sql/plugins/profiles/upgrades/upgrade_db_profiles-1.0.4-1.0.5.sql
@@ -0,0 +1,12 @@
+UPDATE profile_view_action SET icon_url='fa fa-wrench' WHERE  id_action=4;
+UPDATE profile_view_action SET icon_url='fa fa-hand-o-right' WHERE  id_action=3;
+UPDATE profile_view_action SET icon_url='fa fa-edit' WHERE  id_action=1;
+UPDATE profile_view_action SET icon_url='fa fa-trash' WHERE  id_action=2;
+
+UPDATE profile_action SET icon_url="fa fa-edit" WHERE id_action=1;
+UPDATE profile_action SET icon_url="fa fa-trash" WHERE id_action=2;
+UPDATE profile_action SET icon_url="fa fa-user" WHERE id_action=3;
+UPDATE profile_action SET icon_url="fa fa-lock" WHERE id_action=4;
+UPDATE profile_action SET icon_url="fa fa-th-list" WHERE id_action=5;
+UPDATE profile_action SET icon_url="fa fa-group" WHERE id_action=6;
+UPDATE profile_action SET icon_url="fa fa-eye" WHERE id_action=7;
