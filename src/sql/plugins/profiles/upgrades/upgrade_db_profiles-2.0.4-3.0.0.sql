cd9580d08665db5ae78d6b78b521df81d8a0a649
Elysa Jouve <elysa.jouve@paris.fr>
2020-11-20 17:17:50 +0100

    PROFILES-28: templates migration to core v7 macros. Updated icons in init and upgrade SQL files.

---
+++ src/sql/plugins/profiles/upgrades/upgrade_db_profiles-2.0.4-3.0.0.sql
@@ -0,0 +1,12 @@
+UPDATE profile_view_action SET icon_url='wrench' WHERE  id_action=4;
+UPDATE profile_view_action SET icon_url='user-tag' WHERE  id_action=3;
+UPDATE profile_view_action SET icon_url='edit' WHERE  id_action=1;
+UPDATE profile_view_action SET icon_url='trash' WHERE  id_action=2;
+
+UPDATE profile_action SET icon_url="edit" WHERE id_action=1;
+UPDATE profile_action SET icon_url="trash" WHERE id_action=2;
+UPDATE profile_action SET icon_url="user" WHERE id_action=3;
+UPDATE profile_action SET icon_url="lock" WHERE id_action=4;
+UPDATE profile_action SET icon_url="th-list" WHERE id_action=5;
+UPDATE profile_action SET icon_url="users" WHERE id_action=6;
+UPDATE profile_action SET icon_url="eye" WHERE id_action=7;
